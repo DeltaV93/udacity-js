6cc70a2822d8969437a6856f1490bd0bc847c38c
Sharp <vsharp2@zic.pri>
2017-12-20 16:50:02 -0800

    first push

---
+++ main.js
@@ -0,0 +1,32 @@
+var data = {
+    cat1: {
+        name: "Cat 1",
+        image: "cat_picture1.jpg",
+        clicks: 0,
+        active: false
+    },
+    cat2: {
+        name: "Cat 2",
+        image: "cat_picture2.jpeg",
+        clicks: 0,
+        active: false
+    },
+    cat3: {
+        name: "Cat 3",
+        image: "cat_picture3.jpeg",
+        clicks: 0,
+        active: false
+    },
+    cat4: {
+        name: "Cat 4",
+        image: "cat_picture4.jpeg",
+        clicks: 0,
+        active: false
+    }
+};
+
+var listView = {
+    init: function(){
+
+    }
+}
